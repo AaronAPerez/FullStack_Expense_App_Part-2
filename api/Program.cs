36becca89a20384bf1d4653d1acf8beefb63b974
AaronAPerez <aaperez06@gmail.com>
2024-08-21 22:32:45 -0700

    Added User Controller, Models, Services, and all services to Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,15 @@
 using api.Data;
+using api.Services;
+using api.Services.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<ExpenseItemService>();
+builder.Services.AddScoped<PasswordService>();
+
 //for our Azure server
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
